a182279915065be9d2733a755cd63257a592133d
Carl Parker <carlparker.dev@icloud.com>
2024-08-25 22:16:12 -0700

    update favicon asset

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -128,7 +128,7 @@ loaderMarkup = divClass "spinner" $ do
 newHead :: (Prerender js t m, DomBuilder t m) => (R BackendRoute -> Text) -> m (Event t ())
 newHead routeText = do
   el "title" $ text "(BETA) Kadena Chainweaver: Wallet & IDE"
-  elAttr "link" ("rel" =: "icon" <> "type" =: "image/png" <> "href" =: static @"img/favicon/favicon-96x96.png") blank
+  elAttr "link" ("rel" =: "icon" <> "type" =: "image/png" <> "href" =: static @"img/favicon/kadena_green_favicon.png") blank
   meta ("name" =: "description" <> "content" =: "Write, test, and deploy safe smart contracts using Pact, Kadena's programming language")
   meta ("name" =: "keywords" <> "content" =: "kadena, pact, pact testnet, pact language, pact programming language, smart contracts, safe smart contracts, smart contract language, blockchain, learn blockchain programming, chainweb")
   meta ("charset" =: "utf-8")
